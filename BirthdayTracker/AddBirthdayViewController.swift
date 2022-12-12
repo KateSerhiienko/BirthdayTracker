e911000578035985a4b9fdf902b5d8f581e92695
Kateryna Serhiienko <kate.serhiienko@gmail.com>
2022-12-12 17:51:36 +0100

    create actions for buttons Save and Cancel

--- BirthdayTracker/AddBirthdayViewController.swift
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -11,13 +11,24 @@ class AddBirthdayViewController: UIViewController {
     
     @IBOutlet var firstNameTextField: UITextField!
     @IBOutlet var lastNameTextField: UITextField!
-    @IBOutlet var BirthdatePicker: UIDatePicker!
+    @IBOutlet var birthdatePicker: UIDatePicker!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        birthdatePicker.maximumDate = Date()
     }
 
-
+    @IBAction func saveTapped (_ sender: UIBarButtonItem) {
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let birthdate = birthdatePicker.date
+        
+//        let newBirthday = Birthday(firstName: firstName, lastName: lastName, birthdate: birthdate)
+    }
+    
+    @IBAction func cancelTapped (_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
